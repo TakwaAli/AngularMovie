6773ef91ddd829303e0c025d1f50e36cd023c5e4
Takwa <takwaali1520@gmail.com>
2023-08-05 21:40:18 +0300

    pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,18 @@ import { MoviesService } from '../movies.service';
 export class HomeComponent implements OnInit {
   movies:any[]=[]
   img:string='https://image.tmdb.org/t/p/w500'
+  
   constructor(private _movies :MoviesService){
-
+     
   }
   ngOnInit(): void {
-   this._movies.getmovies().subscribe((data)=>{
-     // console.log(data);
-      this.movies=data.results
-   })
+    this.displayMovies(1)
+  }
+  displayMovies(pagenum:number){
+    this._movies.getmovies(pagenum).subscribe((data)=>{
+      // console.log(data);
+       this.movies=data.results
+    })
   }
 
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -10,8 +10,8 @@ export class MoviesService {
   constructor(private _httpClient :HttpClient) { 
   
   }
-  getmovies(): Observable<any> {
-    return  this._httpClient.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=eba8b9a7199efdcb0ca1f96879b83c44&fbclid=IwAR32Px4_3ZTHYF-tjdSOdkN82Esd5XSCl7c0ueF0LR8urOnlJBZ4TJJdf_k`)
+  getmovies(pagenum:number): Observable<any> {
+    return  this._httpClient.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=eba8b9a7199efdcb0ca1f96879b83c44&fbclid=IwAR32Px4_3ZTHYF-tjdSOdkN82Esd5XSCl7c0ueF0LR8urOnlJBZ4TJJdf_k&page=${pagenum}`)
   }
   getMovieDetail(id:string):Observable<any>{
     return this._httpClient.get(`https://api.themoviedb.org/3/movie/${id}?api_key=eba8b9a7199efdcb0ca1f96879b83c44&fbclid=IwAR32Px4_3ZTHYF-tjdSOdkN82Esd5XSCl7c0ueF0LR8urOnlJBZ4TJJdf_k`)
